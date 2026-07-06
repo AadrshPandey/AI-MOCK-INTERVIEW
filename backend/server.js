0637b3c3657f3635b2da5fa8e62235ff787dcef5
Aadrsh Pandey <ap601130@gmail.com>
2026-07-06 10:01:12 +0530

    Cors error solved

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,28 @@ import cors from "cors"
 import dotenv from "dotenv"
 import interviewRouter from "./routes/interview.routes.js"
 
-dotenv.config({
-    path: './env'
-})
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [process.env.CORS_ORIGIN, "http://localhost:5173"];
+
+
+app.use(cors({
+    origin : function(origin, callback){
+        if(!origin) {
+            return callback(null, true);
+        }
+        if(allowedOrigins.includes(origin)){
+            callback(null, true);
+        }
+        else{
+            callback(null, false);
+        }
+    },
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api", interviewRouter);
