4fe0af563e2ebf7b38d8ad89a4a2958702c7fa87
Aadrsh Pandey <ap601130@gmail.com>
2026-07-06 08:54:30 +0530

    Interview routes added to backend

--- backend/routes/interview.routes.js
+++ backend/routes/interview.routes.js
@@ -1,3 +1,12 @@
 import express from "express"
-import { askGemini } from "../gemini.js"
+import { Router } from "express"
+import { generateQuestions } from "../controllers/generateQuestions.controllers.js";
+import { evaluateAnswers } from "../controllers/evaluateAnswers.controllers.js";
 
+const interviewRouter = Router();
+
+interviewRouter.route("/generateQuestions").post(generateQuestions);
+
+interviewRouter.route("/evaluateAnswers").post(evaluateAnswers);
+
+export default interviewRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import interviewRouter from "./routes/interview.routes.js"
 
 dotenv.config({
     path: './env'
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", interviewRouter);
 
 app.listen(PORT, () => {
     console.log("Backend is running on PORT ", PORT);
